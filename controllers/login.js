cad267643ce9f0a35ce5fa6af0bc86ca529d1706
Josh Turan <joshturan@Joshs-Air.lan>
2020-05-19 20:27:06 +0200

    update

--- controllers/login.js
+++ controllers/login.js
@@ -1,35 +1,41 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
-const loginRouter = require('express').Router()
-const User = require('../models/user')
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const loginRouter = require("express").Router();
+const User = require("../models/user");
 
 // @route POST /login
 // @desc Authorize user that is attempting to login
 // @access Private?
 
-loginRouter.post('/', async (request, response) => {
-  const body = request.body
+loginRouter.post("/", async (request, response) => {
+  console.log("login body", request.body);
+  const body = request.body;
 
-  const user = await User.findOne({ username: body.username })
-  const passwordCorrect = user === null
-    ? false : await bcrypt.compare(body.password, user.passwordHash)
+  const user = await User.findOne({ username: body.username });
+  const passwordCorrect =
+    user === null
+      ? false
+      : await bcrypt.compare(body.password, user.passwordHash);
 
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
-      error: 'invalid username or password'
-    })
+      error: "invalid username or password",
+    });
   }
 
   const userForToken = {
     username: user.username,
     id: user._id,
-  }
+  };
 
-  const token = jwt.sign(userForToken, process.env.SECRET)
+  const token = jwt.sign(userForToken, process.env.SECRET);
 
-  response
-    .status(200)
-    .send({ token, username: user.username, name: user.name, status: user.status })
-})
+  response.status(200).send({
+    token,
+    username: user.username,
+    name: user.name,
+    status: user.status,
+  });
+});
 
-module.exports = loginRouter
+module.exports = loginRouter;
--- controllers/users.js
+++ controllers/users.js
@@ -1,50 +1,52 @@
-const bcrypt = require('bcrypt')
-const express = require('express')
-const usersRouter = require('express').Router()
-const fs = require("fs")
-const User = require('../models/user')
-const Profile = require('../models/profile')
-const jwt = require('jsonwebtoken')
-const Avatar = require('../models/avatar')
-const { check, validationResult } = require('express-validator')
-const middleware = require('../utils/middleware')
-const auth = middleware.auth
-
-
+const bcrypt = require("bcrypt");
+const express = require("express");
+const usersRouter = require("express").Router();
+const fs = require("fs");
+const User = require("../models/user");
+const Profile = require("../models/profile");
+const jwt = require("jsonwebtoken");
+const Avatar = require("../models/avatar");
+const { check, validationResult } = require("express-validator");
+const middleware = require("../utils/middleware");
+const auth = middleware.auth;
 
 // @route POST /users
 // @desc Create new user using signup form
 // @access Public
 
-usersRouter.post('/',
+usersRouter.post(
+  "/",
   [
     // check('firstName', 'First name is required').not().isEmpty(),
     // check('lastName', 'Last name is required').not().isEmpty(),
-    check('email', 'Please include a valid email').isEmail()
+    check("email", "Please include a valid email").isEmail(),
   ],
   async (req, res) => {
-
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      console.log(errors)
+      console.log(errors);
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const body = req.body
-
+    const body = req.body;
 
     try {
-
-      let user = await User.findOne({ email: body.email })
+      let user = await User.findOne({ email: body.email });
 
       if (user) {
-        console.log(errors)
-        return res.status(400).json({ errors: [{ msg: 'User already exists with this email', param: 'userExists' }] })
+        console.log(errors);
+        return res.status(400).json({
+          errors: [
+            {
+              msg: "User already exists with this email",
+              param: "userExists",
+            },
+          ],
+        });
       }
 
-
-      const saltRounds = 10
-      const passwordHash = await bcrypt.hash(body.password, saltRounds)
+      const saltRounds = 10;
+      const passwordHash = await bcrypt.hash(body.password, saltRounds);
 
       user = new User({
         firstName: body.firstName,
@@ -54,23 +56,24 @@ usersRouter.post('/',
         status: body.status,
         date: new Date(),
         password: body.password,
-        passwordHash
-      })
+        passwordHash,
+      });
 
-      const savedUser = await user.save()
-      res.json(savedUser)
+      const savedUser = await user.save();
+      res.json(savedUser);
     } catch (err) {
-      console.log('ERRRRR', err)
+      console.log("ERRRRR", err);
     }
-  })
+  }
+);
 
 // @route POST /profile
 // @desc Create or update user profile
 // @access Private
 
-usersRouter.post('/profile', auth, async (req, res, next) => {
-  const body = req.body
-  const user = req.user
+usersRouter.post("/profile", auth, async (req, res, next) => {
+  const body = req.body;
+  const user = req.user;
 
   try {
     const profileFields = {
@@ -82,81 +85,79 @@ usersRouter.post('/profile', auth, async (req, res, next) => {
       website: body.website,
       socialMedia: body.socialMedia,
       user: user._id,
-    }
+    };
 
-    let profile = await Profile.findOne({ user: user.id })
+    let profile = await Profile.findOne({ user: user.id });
 
     if (profile) {
       // UPDATE
-      console.log('FOUND AND UPDATED PROFILE')
+      console.log("FOUND AND UPDATED PROFILE");
 
-      profile = await Profile.findOneAndUpdate({ user: user.id }, { $set: profileFields }, { new: true })
-      return res.json(profile)
+      profile = await Profile.findOneAndUpdate(
+        { user: user.id },
+        { $set: profileFields },
+        { new: true }
+      );
+      return res.json(profile);
     }
 
     // CREATE
-    console.log('CREATED NEW PROFILE')
-    profile = new Profile(profileFields)
+    console.log("CREATED NEW PROFILE");
+    profile = new Profile(profileFields);
 
-
-    const savedProfile = await profile.save()
-    user.profile = user.profile.concat(savedProfile._id)
-    await user.save()
-    res.json(savedProfile)
+    const savedProfile = await profile.save();
+    user.profile = user.profile.concat(savedProfile._id);
+    await user.save();
+    res.json(savedProfile);
   } catch (exception) {
-    next(exception)
+    next(exception);
   }
-})
-
+});
 
 // @route GET /users
 // @desc Get all users, including profiles and avatars
 // @access Public
 
-usersRouter.get('/', async (req, res) => {
-  const users = await User.find({}).populate('profile').populate('avatar')
-  res.json(users.map(u => u.toJSON()))
-  console.log('get users', users)
-})
-
+usersRouter.get("/", async (req, res) => {
+  const users = await User.find({}).populate("profile").populate("avatar");
+  res.json(users.map((u) => u.toJSON()));
+  console.log("get users", users);
+});
 
 // @route GET /:username
 // @desc Get specific user, including profile, avatar, and portfolio uploads
 // @access public
 
-usersRouter.get('/:username', async (req, res, next) => {
-  const user = await User.find({ username: req.params.username }).populate('profile').populate('avatar').populate('upload')
-  console.log(user)
-  res.json(user)
-}
-)
-
+usersRouter.get("/:username", async (req, res, next) => {
+  console.log("get profile", user);
+  const user = await User.find({ username: req.params.username })
+    .populate("profile")
+    .populate("avatar")
+    .populate("upload");
+  console.log(user);
+  res.json(user);
+});
 
 // @route DELETE users/:username
 // @desc Delete profile and user for logged in user
 // @access private
 
-usersRouter.delete('/profile', auth, async (req, res, next) => {
+usersRouter.delete("/profile", auth, async (req, res, next) => {
   try {
-    const user = req.user
-    console.log('user id', user.id)
+    const user = req.user;
+    console.log("user id", user.id);
 
     // Remove profile
-    await Profile.findOneAndRemove({ user: user.id })
-    console.log('Profile deleted')
+    await Profile.findOneAndRemove({ user: user.id });
+    console.log("Profile deleted");
     // Remove user
-    await User.findOneAndRemove({ _id: user.id })
-    console.log('user deleted')
+    await User.findOneAndRemove({ _id: user.id });
+    console.log("user deleted");
 
-    res.json({ msg: 'User deleted ' })
+    res.json({ msg: "User deleted " });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
-)
+});
 
-
-
-
-
-module.exports = usersRouter
+module.exports = usersRouter;
